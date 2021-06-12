7cf7134049f4f04d2d51d03e28a1159d485bbdfb
Jessica Li <jli3273@uwo.ca>
2021-06-12 15:21:08 -0400

    cute messages update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Mixed Messages 
+# Mixed Messages
 The purpose of this program is to display cute and positive messages. This will be accomplished with javascript and displayed with html and css styling. 
 
 ## Process
 1. User clicks on button.
-2. Program randomly picks a compliment from an array and displays it onto the screen.
+2. Program randomly picks a compliment from an array and displays it onto the screen with a cute image.
 3. Processes 1-2 repeat until user decides to close the program. 
+
+## Future Potential Features  
+- Improve shuffling of compliments (e.g. keep random shuffling, but reset when user has seen each compliment once to remove duplicates)
+- Add another messsage generator (e.g. add nav bar for different themes)
--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+const btn = document.getElementById('button');
+const msg = document.getElementById('text');
+const img = document.getElementById('img');
 
 // an array of compliments to be displayed
 const compliments = [
-    "You're super cute", 
-    "Cutebus!",
-    "YAYAYAYAYA CUTIE!",
-    "Hi, I love you."
+    {
+        compliment: "You're super cute", 
+        image: "./images/givelove.gif"
+    }, 
+    {
+        compliment: "Love you!",
+        image: "./images/heartheart.gif"
+    }, 
+    {
+        compliment: "Cutie :)",
+        image: "./images/heart.gif"
+    },
+    {
+        compliment: "uwu?",
+        image: "./images/tenor.gif"
+    },
+    {
+        compliment: "Listen up, you're amazing!",
+        image: "./images/shout.gif"
+    },
+    {
+        compliment: "gogogogogo :D",
+        image: "./images/cheer.gif"
+    }
 ];
 
 // randomly picks a compliment from array to display 
@@ -12,10 +35,23 @@ const numGenerator = () => {
     return randomNum = Math.floor(Math.random() * compliments.length);
 }
 
+/*
 // picks compliment to display 
 const displayCompliment = () => {
     let num = numGenerator();
-    return compliments[num];    
+    //return compliments[num];
+    document.getElementById("text").innerHTML=compliments[num];
+}*/
+
+const displayCompliment = () => {
+    let num = numGenerator();
+    let quote = compliments[num]
+    msg.innerHTML = quote.compliment;
+    //message.innerHTML = compliments[num];
+    //img.src = compliments[num];
+    img.src=quote.image;
 }
 
-console.log(displayCompliment());
+btn.addEventListener('click', () => {
+    displayCompliment;
+});
